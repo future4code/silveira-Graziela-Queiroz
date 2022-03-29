084ce16fc81812e54583caed01578770c1712798
graziela-queiroz <graziela-queiroz@hotmail.com>
2022-03-28 21:51:57 -0300

    ajuste select

--- modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa1.js
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa1.js
@@ -12,20 +12,29 @@ class Etapa1 extends React.Component {
     render() {
         return (
             <Form1>
-                <h1>ETAPA 1 - DADOS GERAIS</h1>
-
-                <p>1.Qual o seu nome?</p>
-                <input></input>
-
-                <p>2. Qual sua idade? </p>
-                <input></input>
-
-                <p>Qual seu email?</p>
-                <input></input>
-
-                <p>Qual a sua escolaridade?</p>
-                <input></input>
-
+                <h3>ETAPA 1 - DADOS GERAIS</h3>
+                <div>
+                    <p>1.Qual o seu nome?</p>
+                    <input value=""></input>
+                </div>
+                <div>
+                    <p>2. Qual sua idade? </p>
+                    <input value=""></input>
+                </div>
+                <div>
+                    <p>3.Qual seu email?</p>
+                    <input value=""></input>
+                </div>
+                <div>
+                    <p>4.Qual a sua escolaridade?</p>
+                    <input value=""></input>
+                    <select>
+                        <option value="Ensino médio incompleto">Ensino medio incompleto</option>
+                        <option value="Ensino médio completo">Ensino medio completo</option>
+                        <option value="Ensino superior incompleto">Ensino superior incompleto</option>
+                        <option value="Ensino superior completo">Ensino superior completo</option>
+                    </select>
+                </div>
             </Form1>
         );
     }
--- modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa3.js
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa3.js
@@ -17,8 +17,12 @@ class Etapa3 extends React.Component {
                 <p>5.Por que você não terminou um curso de graduação?</p>
                 <input></input>
 
-                <p> Você fez algum curso complementar? option=(["Nenhum","Curso técnico","Curso de ingles"]) </p>
-                <input></input>
+                <p> Você fez algum curso complementar?</p>
+                <select>
+                        <option value="Nenhum">Nenhum</option>
+                        <option value="Curso técnico">Curso técnico</option>
+                        <option value="Curso de ingles">Curso de ingles</option>
+                </select>
 
             </Form3>
         );
