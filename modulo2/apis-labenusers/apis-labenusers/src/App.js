ab6ec7268c693868c2756761bf7b4a385cb00d71
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-04-18 19:14:45 -0300

    react hooks

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import TelaCadastro from './components/TelaCadastro';
+import TelaListaUsuario from './components/TelaListaUsuario';
+
+export default class App extends React.Component{
+  state = {
+    telaAtual: "cadastro"
+  }
+
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+    case "cadastro":
+      return <TelaCadastro irParaLista={this.irParaLista} />
+    case "lista":
+      return <TelaListaUsuario irParaCadastro={this.irParaCadastro} />
+    default:
+      return <div>Erro! Pagina nao encontrada</div>
+    }     
+  }
+
+  irParaCadastro = () => {
+    this.setState({ telaAtual: "cadastro" })
+  }
+
+  irParaLista = () => {
+    this.setState({ telaAtual: "lista" })
+  }
+
+  render(){
+    return (
+      <div>
+        {this.escolheTela()}
+      </div>
+    )
+  }
+}
+
+
