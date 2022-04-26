01c350aa4d8a43a28ea69c48fbe500d7c047d51e
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-04-25 22:09:59 -0300

    diagrama wireframe

---
+++ modulo3/projeto-labex/projeto-labex/src/pages/Home.js
@@ -0,0 +1,23 @@
+// import styled from 'styled-components'
+// import { createGlobalStyle } from 'styled-components';
+// import React from 'react';
+// import axios from 'axios';
+// import Home from './pages/Home'
+
+// const GlobalStyle = createGlobalStyle` 
+//   * {    
+//     margin: 0;
+//     padding: 0;
+//     box-sizing: border-box;
+//     outline: 0;
+//   }
+
+
+// export default function Home(){
+//   return (
+//     <div>
+//         <Home/>
+//     </div>
+//   );
+     
+// }
