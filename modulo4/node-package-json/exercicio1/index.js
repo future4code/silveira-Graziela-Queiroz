f17f2c269dcb68a2412227143184980097711827
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-05-24 19:00:41 -0300

    node e package.json

---
+++ modulo4/node-package-json/exercicio1/index.js
@@ -0,0 +1,10 @@
+  //Exercicio 1
+
+   //a) 
+   process.argv[2]
+
+   //b)
+
+ const nome = "Graziela"
+ const age = Number (40)
+ console.log(`Olá, ${nome}!Você tem ${age} anos. Em sete anos você terá ${age+7}.`)  
---
+++ modulo4/node-package-json/exercicio2/index.js
@@ -0,0 +1,12 @@
+// Exercicio 2
+const soma = Number (process.argv[2])
+console.log(soma + soma)
+
+const subtrair = Number (process.argv[2])
+console.log(subtrair - 2)
+
+const multiplicar = Number (process.argv[2])
+console.log(multiplicação *50)
+
+const divisao = Number (process.argv[2])
+console.log(divisao /2)
