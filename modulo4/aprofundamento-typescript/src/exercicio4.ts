f8edbaad511f53044af7f7bebfc19feb8bda51c3
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-05-26 21:52:26 -0300

    terminando o aprofundamento-typescript

--- modulo4/aprofundamento-typescript/src/exercicio4.ts
+++ modulo4/aprofundamento-typescript/src/exercicio4.ts
@@ -21,12 +21,25 @@ const pokemon3: pokemon = {
   types: "Water",
   healthPoints: 35
 }
-// b) O tsc executa a compilação.
+console.log(pokemon1, pokemon2, pokemon3)
+
+// b) O npm executa a compilação dentro do arquivo package.json , que tem os scripts que rodam no terminal.
+
+// o uso do arquivo tsc -- Init e em sefuida descomento o que foi pedido seguido o slide.
+
+Resposta:
+//"target": "es2016",                                  /* Set the JavaScript language version for emitted JavaScript and include compatible library declarations
+//"module": "commonjs",                                /* Specify what module code is generated.
+//  "rootDir": "./src",                                  /* Specify the root folder within your source files. */
+// "sourceMap": true,                                /* Create source map files for emitted JavaScript files. */
+//"outDir": "./build",                                   /* Specify an output folder for all emitted files. */
+//"removeComments": true,  
+//"forceConsistentCasingInFileNames": true,            /* Ensure that casing is correct in imports. */
+/* Disable emitting comments. */
+//"esModuleInterop": true,                             /* Emit additional JavaScript to ease support for importing CommonJS modules. This enables 'allowSyntheticDefaultImports' for type compatibility. */
+//"strict": true,                                      /* Enable all strict type-checking options. */
+//"noImplicitAny": true, 
+//"skipLibCheck": true                                 /* Skip type checking all .d.ts files. */
+/* Enable error reporting for expressions and declarations with an implied 'any' type. */
 
 
-
-//c)
-
-
-
-//d)
