b3099eecfb3a41fe6e1f01ec16b2de75f69e159b
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-06-02 23:12:12 -0300

    api rest 1

--- modulo4/api-rest/src/index.ts
+++ modulo4/api-rest/src/index.ts
@@ -1,8 +1,9 @@
 // import express from "express";
+import { Console } from "console";
 import cors from "cors";
 import express, { Request, Response } from "express";
+import { CreateProgram } from "typescript";
 import { users } from './data'
-//import {type} from
 
 
 const app = express();
@@ -14,8 +15,22 @@ app.use(cors());
 
 //Exercicio 1
 
-app.get("/users", (request: Request, response: Response) => {
-        response.status(200).send({users})
+app.get("/users", (req: Request, res: Response) => {
+    let codeError: number = 400;
+    try {
+        if (users) { // se existir uma lista de usuario? (users)
+            res.status(200).send(users)// retorna a lista pra mim 
+        } else { //se não cai no error
+            codeError = 404
+            throw new Error('Lista de usuário não encontrada')
+        }
+
+    } catch (error: any) {
+        res.status(codeError).send({ message: error.message })
+    }
+
+
+
 })
 
 //a) metodo GET
@@ -23,49 +38,68 @@ app.get("/users", (request: Request, response: Response) => {
 
 //Exercicio 2
 
-// query parameters "/user" postman "/users?id+1"
-app.get('/buscarfiltro/:type',(request:Request, responde:Response) => {
-try {
-   const type = request.params.type
-   const filtrar = users.filter((user) => {
-       if (user.type === type) {
-           return user
-       }
-   })
-   if (!type) {
-       codeError = 404
-       throw new Error ('digitar um parametro')
-   }
-    if(!filtrar.length){
-       codeError = 404
-       throw new Error ('lista de usuario não encontrada')
+app.get("/users/:type", (req: Request, res: Response) => {
+    let codeError: number = 400;
+
+    try {
+        const type = req.params.type
+        const filtrar = users.filter((user) => {
+            if (user.type === type) {
+                return user
+            }
+        })
+
+        if (!type) {
+            codeError = 404
+            throw new Error('É preciso digitar algum parametro')
+
+        }
+        if (!filtrar.length) {
+            codeError = 404
+            throw new Error('Lista de usuário não encontrada')
+        }
+        res.status(200).send(filtrar)
+
+    } catch (error: any) {
+        res.status(codeError).send({ message: error.message })
+
     }
-    res.status(200).send(filtrar)  
-} catch (error: any) {
-    responde.status(codeError).send ({message: error.message})
-}
-
-
-
-
-
-// Exercicio 3
-// app.get("/users/userfilter",(request:Request,response:Response) =>{
-//  let errorCode = 400
-
-//  const usersFilter: any = []
- 
-
-
-
-
-
 
 })
 
+// exercicio 3
 
+// app.get("/createuser", (req: Request, res: Response) => {
+//     let codeError: number = 400;
+//     try {
+//         const name = (req.query.name) as string
+//         const filtrarUsuario: createuser | undefined = users.find(user => user.name === name)
+//         if (!filtrarUsuario) {
+//             codeError = 404
+//             throw new Error('Usuário não encontrado por favor digite um nome de usuário!')
+//         }
+//         res.status(200).send(filtrarUsuario)
+//     } catch (error: any) {
+//         res.status(codeError).send({ message: error.message })
+//     }
+// })
+//exercicio 4
 
-
+// app.put("/users1", (req: Request, res: Response) => {
+//     let codeError: number = 400;
+//     try {
+//         const { id, name, email, type, age } = req.body
+//         if (!id || !name || !email || !type || !age) {
+//             let errorCode = 422
+//             throw new Error('Por favor verifique se os campos foram preenchidos corretamente')
+//         }
+//         const newUser: users1 = {
+//             id,
+//             name,
+//             email,
+//             type,
+//             age
+//         }
 
 
 
