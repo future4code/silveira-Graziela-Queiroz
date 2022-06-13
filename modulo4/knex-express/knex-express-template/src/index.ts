5b2c71048af4cf09b8378fe8fdbbc9ed0f107844
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-06-12 23:24:44 -0300

    exercicios 4 e 5

--- modulo4/knex-express/knex-express-template/src/index.ts
+++ modulo4/knex-express/knex-express-template/src/index.ts
@@ -13,190 +13,183 @@ app.use(express.json());
 app.use(cors());
 
 
-export type Cadastro = {
-  name: string,
-  nickname: string
-  email: string
-}
-
-const cadastroList: Array<Cadastro> = []
-
-
-export type tarefas = {
-  taskId: string,
-  title: string,
-  description: string,
-  limitDate: string,
-  status: string,
-  userPedinte: string,
-  creatorUserId: string,
-  creatorUserNickname: string
-}
-
-const listaTarefas: Array<tarefas> = [
-  {
-    taskId: "001",
-    title: "Criar banco dos alunos",
-    description: "Devemos criar o banco dos alunos para o módulo do backend",
-    limitDate: "04/05/2020",
-    status: 'feito',
-    userPedinte: '',
-    creatorUserId: "001",
-    creatorUserNickname: "astrodev"
-  }
-]
-
-
-
-// const afazerList: Array<Afazeres> = [
-//   { taskId: 001, id: 1, title: "Criar banco dos alunos", discription: 'Devemos criar tabela' },
-//   { taskId: 002, id: 2, title: "", completed: 'false' },
-//   { taskId: 003, id: 3, title: "", completed: 'false' },
-//   { taskId: 004, id: 4, title: "", completed: 'true' },
-//   { taskId: 005, id: 5, title: "", completed: 'false' },
-// ]
-//{taskId: "001" title : "Criar banco dos alunos", description:""...}
-
-
-
 // Exercicio 1
 
-// Criar Usuario
+// criar usuario
+const createUsuario = async ( nome: string, nickname: string, email: string): Promise<any> => {
+  const result = await connection("Usuario")
+    .insert({
+      nome: nome,
+      nickname: nickname,
+      email: email,
+    })
 
-// Como gero o id do usuario?
+  return result
+}
 
-
-app.post("/user", (req: Request, res: Response) => {
+//  criar endpoint e validar
+app.post('/user', async (req: Request, res: Response) => {
   try {
-    const { name, nickname, email } = req.body
-    let type = req.body.type as string
 
-    if (!name || !nickname || !email || !type) {
+    const { nome, nickname, email } = req.body
+    if (!nome || !nickname || !email) {
       throw new Error("Missing data in body to create user")
     }
 
-    if (typeof name !== "string") {
-      throw new Error("nome invalido")
+    await createUsuario( nome, nickname, email)
+
+    res.status(201).send("Usuario criado com sucesso!")
+  } catch (error: any) {
+    if (res.statusCode == 200) {
+      res.status(500).send(error.sqlMessage || error.message)
+    } else {
+      res.status(res.statusCode).send(error.message)
     }
-
-    if (typeof nickname !== "string") {
-      throw new Error("Apelido invalido")
-    }
-
-    if (typeof email !== "number") {
-      throw new Error("email invalido")
-    }
-
-    // eu preciso realmente confirmar o email com o if?
-    // user.forEach(use => {
-    //   if (use.email === email) {
-    //     throw new Error("Email já existe")
-    //   }
-    // })
-
-    const newCadastro: Cadastro = {
-      name: req.body.name,
-      nickname: req.body.nickname,
-      email: req.body.email,
-    }
-
-    cadastroList.push(newCadastro)
-
-    res.status(200).send({message: cadastroList})
-
-  } catch (error) {
-    res.status(400).send(error)
   }
 })
 
 
 // Exercicio 2
+// Função para pegar usuario por id
+const getUsuarioById = async (id: string): Promise<any> => {
+  const result = await connection
+    .select('id', 'nickname')
+    .from('Usuario')
+    .where('id', id)
 
+  return result
+}
 
-// app.get("/users/:id", (req: Request, res: Response) => {
-//   try {
-//     let id = Number(req.params.id)
-//     let name = (req.query.name)
-//     if (isNaN(id)) {
-//       throw new Error("Invalid id")
-//     }
-//     return res.status(200).end()
-//     res.status(204).send("Usuario não encontrado")
-//   } catch (error) {
-//     res.status(400).send(error.message)
-//   }
-// })
+app.get('/user/:id', async (req: Request, res: Response) => {
+  try {
 
-// o endpoint deve encontrar um erro se não encontrar o usuario.
+    const id: string = req.params.id
+
+    if (!id) {
+      res.statusCode = 400
+      throw new Error('Id não informado')
+    }
+
+    const result = await getUsuarioById(id)
+
+    res.status(200).send(result[0])
+
+  } catch (error: any) {
+    if (res.statusCode == 200) {
+      res.status(400).send(error.sqlMessage || error.message)
+    } else {
+      res.status(res.statusCode).send(error.message)
+    }
+  }
+})
+
 
 // Exercicio 3
+// Função de atualizar nome, nickname
+const updateUsuario = async (id: string, nickname: string, nome: string): Promise<any> => {
+  const result = await connection("Usuario")
+    .update({
+      nome: nome,
+      nickname: nickname
+    })
+    .where('id', id)
+
+  return result
+}
+
+app.put('/user/edit/:id', async (req: Request, res: Response) => {
+  try {
+
+    const id: string = req.params.id
+    const { nome, nickname } = req.body
+
+    if (!nome || !nickname) {
+      res.statusCode = 400
+      throw new Error('nome/nickname não informado')
+    }
+
+    await updateUsuario(id, nome, nickname)
+
+    res.status(200).send("Usuario atualizado com sucesso!")
+
+  } catch (error: any) {
+    if (res.statusCode == 200) {
+      res.status(500).send(error.sqlMessage || error.message)
+    } else {
+      res.status(res.statusCode).send(error.message)
+    }
+  }
+})
 
 
-//     app.put("/user/edit/:id", (req: Request, res: Response) => {
-//       try {
-//         let edit = (req.body.edit)
-//         if (isNaN(id)) {
-//           throw new Error("Invalid id")
-//         }
+// Exercicio 4
+const criarTarefa = async (title: string, description: string, limitDate: Date, creatorUserId: string) => {
+  const result = await connection("Tarefa")
+    .insert({
+      title: title,
+      description: description,
+      limit_date: limitDate,
+      creator_user_id: creatorUserId
+    })
 
-//         users.forEach(user => {
-//           if (user.id === id) {
-//             user.name += "-ALTERADO"
-//             return res.status(200).end()
-//           }
-//         })
-//         res.status(204).send("Codigo não Alterado ")
-//       } catch (error) {
-//         res.status(400).send(error.message)
-//       }
-//     }
-// }
-
-// Exercicio 4 
-
-// app.post("/task", (req: Request, res: Response) => {
-//       try {
+  return result
+}
 
 
-//       } catch (error) {
+app.post('/task', async (req: Request, res: Response) => {
+  try {
 
-//       }
+    const { title, description, limitDate, creatorUserId } = req.body
+    if (!title || !description || !limitDate || !creatorUserId) {
+      throw new Error("Nenhum campo pode estar vazio!")
+    }
 
+    await criarTarefa(title, description, limitDate, creatorUserId)
 
-//     }
-
-
+    res.status(201).send("Tarefa criado com sucesso!")
+  } catch (error: any) {
+    if (res.statusCode == 200) {
+      res.status(400).send(error.sqlMessage || error.message)
+    } else {
+      res.status(res.statusCode).send(error.message)
+    }
+  }
+})
 
 
 // Exercicio 5
+// Função para Tarefa pelo id
+const getTarefaById = async (id: string): Promise<any> => {
+  const result = await connection
+  .select('id','title','description','status','limit_date','creator_user_id','creator_user_nickname')
+  .from('Tarefa')
+  .where('id', id)
 
-// app.get("/task/:id", (req: Request, res: Response) => {
-//       try {
-//         let task = (req.query.task)
+  return result
+}
 
-//       } catch (error) {
+app.get('/task/:id', async (req: Request, res: Response) => {
+  try {
 
-//       }
+    const id: string = req.params.id
 
+    if (!id) {
+      res.statusCode = 400
+      throw new Error('Id não informado')
+    }
 
+    const result = await getTarefaById(id)
 
+    res.status(200).send(result[0])
 
-
-//     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  } catch (error: any) {
+    if (res.statusCode == 200) {
+      res.status(400).send(error.sqlMessage || error.message)
+    } else {
+      res.status(res.statusCode).send(error.message)
+    }
+  }
+})
 
 
 
@@ -210,4 +203,4 @@ const server = app.listen(process.env.PORT || 3003, () => {
   } else {
     console.error(`Failure upon starting server.`);
   }
-});
+})
