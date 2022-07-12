91fdd4be1cad1c00e4b4bdc334f6f5b3e77b523a
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-07-11 21:30:21 -0300

    testes-automatizados

---
+++ modulo5/testes-automatizados/src/tests/index.test.ts
@@ -0,0 +1,10 @@
+import {isEven, connection} from "../src"
+
+describe("testando o arquivo index.js", () => {
+    test("testando a função isEven", () => {
+        const result = isEven(2)
+
+        expect(result).toBe(true)
+
+        expect(isEven(3)).toBe(false)
+    })
