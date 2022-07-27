9e89c690337b8397f3b6e7e5f2747e2ab1e5014d
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-07-27 17:34:34 -0300

    contruindo-pagina-detalhes

---
+++ modulo6/rodada-case-02/promobit/src/pages/movie/Movie.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/header/Header';
+import axios from 'axios';
+import { base_url, BASE_URL, BASE_URL2 } from '../../constants/url';
+import CardMovie from '../../components/cardMovie/CardMovie'
+import { ContainerCardMovie, DivGenres, DivGeral } from './style';
+//import { useNavigate } from 'react-router-dom';
+import CardMovieButton from '../../components/ButtonGenreMovie/CardMovieButton';
+
+function Movie() {
+    const [movies, setMovies] = useState([]);
+
+    //const navigate = useNavigate();
+
+    const getMovie = async () => {
+        await axios.get(`${BASE_URL2}`) // retorna os filmes 
+            .then((res) => {
+                setMovies(res.data);
+                //console.log(res.data)
+            })
+            .catch((err) => {
+                console.log('erro', err);
+            });
+    };
+
+    useEffect(() => {
+        getMovie();
+    }, []);
+
+    const filmes = movies.results?.map((filme) => {
+        return (
+            <CardMovie
+                id={filme.id}
+                key={filme.id}
+                foto={<img component="img" height="240" src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt="Poster" />}
+                movie={filme.title}
+                date={filme.release_date} 
+            />
+        );
+    });
+
+    return (
+        <DivGeral>
+            <Header />
+
+            <ContainerCardMovie>
+                {filmes}
+            </ContainerCardMovie>
+
+        </DivGeral>
+    );
+}
+
+export default Movie;
